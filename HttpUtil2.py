bf6f4270afefcf63ac3ae47f726db34cc5228e53
Andres De Vera <andres.devera@cancilleria.mrree.gub.uy>
2023-06-26 10:56:05 -0300

    httputils 2

---
+++ HttpUtil2.py
@@ -0,0 +1,31 @@
+import requests
+import hashlib
+import hmac
+
+def getSign(params, secretKey):
+    sign = ''
+    for key in params.keys():
+        value = str(params[key])
+        sign += key + '=' + value + '&'
+    sign = sign[:-1]
+
+    mySign = hmac.new(secretKey.encode(), sign.encode(), hashlib.sha512).hexdigest()
+    return mySign
+
+def httpGet(url, resource, params=''):
+    full_url = f"{url}/{resource}/{params}"
+    response = requests.get(full_url)
+    data = response.json()
+    return data
+
+def httpPost(url, resource, params, apiKey, secretKey):
+    full_url = f"{url}/{resource}"
+    headers = {
+        "Content-type": "application/x-www-form-urlencoded",
+        "KEY": apiKey,
+        "SIGN": getSign(params, secretKey)
+    }
+    response = requests.post(full_url, data=params, headers=headers)
+    data = response.json()
+    params.clear()
+    return data
