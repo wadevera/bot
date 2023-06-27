8f52a180ed8d5eb9a9e2e18f22b7d4fa06c82e16
Andres De Vera <andres.devera@cancilleria.mrree.gub.uy>
2023-06-27 14:20:26 -0300

    con bot1

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,8 @@
 # A very simple Flask Hello World app for you to get started with...
 #from FuturosBot import FuturosBot
-#from gateBot import GateBot
+from gateBot import GateBot
 from datetime import datetime
-#import Configuracion
+import Configuracion
 
 from flask import request
 from flask import Flask
@@ -20,22 +20,22 @@ def hello_world():
 
 
 
-#@app.route('/bot1', methods=['POST'])
-#def bot():
-#    parametro = str(request.data, 'UTF-8').lower()
-#    api_trade_url = Configuracion.API_TRADE_URL
-#    f = open("salida.txt", "a")
-#    f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+ " -> " + parametro + " api:" + api_trade_url + "\n")
-#    f.close()
-#
-#    # Tomar los valores de configuración del archivo Configuracion.py
-#    
-#    api_key = Configuracion.GATE_API_KEY
-#    secret_key = Configuracion.GATE_SECRET_KEY
-#
-#    # Modificación en la creación de la instancia de GateBot
-#    bot = GateBot(api_trade_url, api_key, secret_key)
-#
-#    bot.Entrar(parametro)
-#
-#    return 'ok'
+@app.route('/bot1', methods=['POST'])
+def bot():
+    parametro = str(request.data, 'UTF-8').lower()
+    api_trade_url = Configuracion.API_TRADE_URL
+    f = open("salida.txt", "a")
+    f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+ " -> " + parametro + " api:" + api_trade_url + "\n")
+    f.close()
+
+    # Tomar los valores de configuración del archivo Configuracion.py
+    
+    api_key = Configuracion.GATE_API_KEY
+    secret_key = Configuracion.GATE_SECRET_KEY
+
+    # Modificación en la creación de la instancia de GateBot
+    bot = GateBot(api_trade_url, api_key, secret_key)
+
+    bot.Entrar(parametro)
+
+    return 'ok'
